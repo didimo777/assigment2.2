5963051b542b76b619a22d881f528134f20ff203
didimo777 <momenzanovadilnaz@gmail.com>
2026-01-04 02:58:42 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        Servicable s1 = new Car("Toyota", 2018, 20000, 4);
+        Servicable s2 = new Bus("Mercedes", 2015, 60000, 50);
+
+        s1.performService();
+        System.out.println(s1.getServiceIntervalKm());
+
+        s2.performService();
+        System.out.println(s2.getServiceIntervalKm());
+    }
 }
